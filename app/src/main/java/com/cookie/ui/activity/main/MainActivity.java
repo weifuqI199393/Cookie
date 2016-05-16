4a0d7a3234f5ded69f35216e4543f50dd596f90a
魏福气 <weifuqi@weifuqideMBP.lan>
2016-05-16 14:23:04 +0800

    Initial commit

---
+++ app/src/main/java/com/cookie/ui/activity/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookie.ui.activity.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cookie.ui.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
